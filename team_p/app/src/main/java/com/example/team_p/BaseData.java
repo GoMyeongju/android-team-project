4b68dee6f1b82f30daa4d6e780dfeee409946a7f
전세운 <wjstpdns2015@naver.com>
2019-12-05 01:25:16 +0900

    BaseData 정의 (디비와 주고받는 데이터형의 부모 클래스)
    UserData 정의

---
+++ team_p/app/src/main/java/com/example/team_p/BaseData.java
@@ -0,0 +1,26 @@
+package com.example.team_p;
+
+import java.util.ArrayList;
+
+public class BaseData {
+    private ArrayList<String> keys;
+    private ArrayList<Object> values;
+
+    protected BaseData() {
+        this.keys = new ArrayList<>();
+        this.values = new ArrayList<>();
+    }
+
+    protected void addHash(String key, Object value){
+        keys.add(key);
+        values.add(value);
+    }
+
+    public ArrayList<String> getKeys() {
+        return keys;
+    }
+
+    public ArrayList<Object> getValues() {
+        return values;
+    }
+}
